b99ff5cda61c68a3afb728572da8c70642894bba
Robert Jaskowski <robertjanjaskowski@gmail.com>
2021-05-11 02:05:10 +0200

    init

---
+++ StoryWriter/StoryWriter/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using StoryWriter.Services;
+using StoryWriter.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StoryWriter
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ StoryWriter/StoryWriter/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using StoryWriter.ViewModels;
+using StoryWriter.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace StoryWriter
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
